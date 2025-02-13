fb64e1fa1351a315fc190469eef2e653e12aacfc
Chetan Kolhe <Chetankolhe1020@gmail.com>
2025-02-14 00:09:55 +0530

    13/2/2025 Java Input Output

---
+++ IoDemo.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+class ioDemo{
+
+	public static void main (String [] args){
+
+		Scanner sc = new Scanner(System.in);
+		String firstInputInJava = sc.next();
+		System.out.println(firstInputInJava);
+
+
+	}
+
+
+
+}
---
+++ forDemo.java
@@ -0,0 +1,17 @@
+
+
+
+
+class forDemo {
+
+	public static void main (String [] args){
+
+		for(int i =0;i<20;i++){
+			
+			System.out.println("Hello World " + i);
+
+		
+		}
+	}
+
+}
