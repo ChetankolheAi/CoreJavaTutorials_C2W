84af520a127a582fe47f17f879145ece36571364
Chetan Kolhe <Chetankolhe1020@gmail.com>
2025-02-14 00:26:26 +0530

    Change_Directory

--- BasicsOfJava/whileDemo.java
+++ BasicsOfJava/whileDemo.java
@@ -1,13 +1,14 @@
+package BasicsOfJava;
 
 import java.util.*;
 class whileDemo {
     
     public static void main(String [] args){
 
-        Scanner sc1 = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter Any Number");
-        int input = sc1.nextInt();
+        int input = sc.nextInt();
         int i = 1;
 
         System.out.println("Printing All Number From 1 to "+ input);
